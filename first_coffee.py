2eaf1a05682a5c294a55d416e671d569e1fe274a
Jupieter <63549139+Jupieter@users.noreply.github.com>
2022-07-15 17:34:18 +0200

    data request from main to first_coffe

--- first_coffee.py
+++ first_coffee.py
@@ -18,10 +18,49 @@ class FirstCoffe(MDCard): # the.boss@staff.com    Enter1
 	def __init__(self, **kwargs):
 		super(FirstCoffe, self).__init__(**kwargs)
 		print('fk_test')
-		# Clock.schedule_interval(self.tik, 3)
+		magam = self
+		x = self.ids
+		print(x)
+		Clock.schedule_once(magam.load_data, 0)
+		Clock.schedule_interval(magam.load_data, 1) 
 	
-	def tik(selftik):
+	def tik(self, *args):
 		print('tik-tak')
+	
+	def load_token(self, *args):
+		conn = sqlite3.connect('coffe_app.db')
+		active_tok = conn.execute("SELECT act_token from act_tokens")
+		for row in active_tok:
+			active_token = row[0]
+			print ("token = ", active_token)
+		print('recycle 3')
+		# Clock.schedule_interval(print('tik-tak'), 3)
+		return active_token
+
+	def load_data(self, *args):
+		print('recycle 2')
+		active_token = self.load_token()
+		print('LOG Token', active_token)
+		token_str = 'Token ' + active_token
+		hd_token = {'Authorization':token_str}
+		if active_token != 'Empty':
+			store = requests.get('http://127.0.0.1:8000/c_app/todaytcoffee/', headers=hd_token).json()
+			print(store)
+			list_data = []
+			for item in store:
+				list_data.append({'text': item['c_make_date']})
+			first_coffe = list_data[0]['text']
+			print(first_coffe)
+			# self.data = first_coffe
+			# print(self.dat			
+			fc_date = first_coffe[0:10]
+			print(fc_date)
+			fc_hour = first_coffe[11:13]
+			fc_min = first_coffe[14:16]
+			print(fc_hour,':',fc_min)
+			date_time = [fc_date, fc_hour, fc_min]
+			print(date_time)
+
 
 
 
--- main.py
+++ main.py
@@ -47,50 +47,6 @@ class DrawerList(ThemableBehavior, MDList):
                 break
         instance_item.text_color = self.theme_cls.primary_color
 
-#recycle view for home screen
-class MyRecycleView(RecycleView):
-    print('Recycle 1')
-    def __init__(self, **kwargs):
-        super(MyRecycleView, self).__init__(**kwargs)
-        print('Recy Token', active_token)
-        Clock.schedule_interval(self.load_data, 3)
-
-
-    def load_token(self, *args):
-        conn = sqlite3.connect('coffe_app.db')
-        active_tok = conn.execute("SELECT act_token from act_tokens")
-        for row in active_tok:
-            active_token = row[0]
-            print ("token = ", active_token)
-        print('recycle 3')
-        # Clock.schedule_interval(print('tik-tak'), 3)
-        return active_token
-
-    def load_data(self, *args):
-        print('recycle 2')
-        active_token = self.load_token()
-        print('LOG Token', active_token)
-        token_str = 'Token ' + active_token
-        hd_token = {'Authorization':token_str}
-        if active_token != 'Empty':
-            store = requests.get('http://127.0.0.1:8000/c_app/todaytcoffee/', headers=hd_token).json()
-            print(store)
-            list_data = []
-            for item in store:
-                list_data.append({'text': item['c_make_date']})
-            first_coffe = list_data[0]['text']
-            print(first_coffe)
-            # self.data = first_coffe
-            # print(self.data)
-
-            fc_date = first_coffe[0:10]
-            print(fc_date)
-            fc_hour = first_coffe[11:13]
-            fc_min = first_coffe[14:16]
-            print(fc_hour,':',fc_min)
-            date_time = [fc_date, fc_hour, fc_min]
-            print(date_time)
-
 
 class TestNavigationDrawer(MDApp):
     def build(self):
